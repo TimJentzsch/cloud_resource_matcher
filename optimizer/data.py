3f83bed1dc8399a35ed0f07eac856be963c473bf
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2022-12-18 20:53:27 +0100

    Split the data into multiple classes and add functions to add the data to the model

--- optimizer/data.py
+++ optimizer/data.py
@@ -10,16 +10,45 @@ Cost = float
 
 
 @dataclass
-class Data:
+class BaseData:
+    # The available virtual machines
     virtual_machines: List[VirtualMachine]
-    virtual_machine_min_ram: Dict[Service, int]
-    virtual_machine_min_cpu_count: Dict[Service, int]
 
-    regions: List[Region]
-
+    # The available services
     services: List[Service]
-    service_regions: Dict[Service, Region]
-    service_cloud_service_providers: Dict[Service, CloudServiceProvider]
+
+    # The services that are applicable for each virtual machine
+    virtual_machine_services: Dict[VirtualMachine, Service]
+
+    # The base cost for each service
     service_base_costs: Dict[Service, Cost]
+
+
+@dataclass
+class PerformanceData:
+    # The minimum amount of RAM each virtual machine requires
+    virtual_machine_min_ram: Dict[VirtualMachine, int]
+
+    # The minimum mount of vCPUs each virtual machine requires
+    virtual_machine_min_cpu_count: Dict[VirtualMachine, int]
+
+    # The amount of RAM each service has
     service_ram: Dict[Service, int]
+
+    # The amount of vCPUs each service has
     service_cpu_count: Dict[Service, int]
+
+
+@dataclass
+class MultiCloudData:
+    # The available cloud service providers
+    cloud_service_providers: List[CloudServiceProvider]
+
+    # The services each cloud service provider offers
+    cloud_service_provider_services: Dict[CloudServiceProvider, List[Service]]
+
+    # The minimum number of cloud service providers that have to be used
+    min_cloud_service_provider_count: int
+
+    # The maximum number of cloud service providers that can be used
+    max_cloud_service_provider_count: int
--- optimizer/model.py
+++ optimizer/model.py
@@ -1,46 +1,113 @@
-from typing import Dict
+from typing import Dict, Optional, Self
 
-from pulp import LpVariable, LpProblem, LpMinimize, lpSum
+from pulp import LpVariable, LpProblem, LpMinimize, lpSum, LpAffineExpression, LpBinary
 
-from optimizer.data import Data, VirtualMachine, Service
+from optimizer.data import (
+    BaseData,
+    VirtualMachine,
+    Service,
+    PerformanceData,
+    MultiCloudData,
+)
 
 
 class Model:
     """The model for the cloud computing cost optimization problem."""
-    data: Data
-    prob: LpProblem
-    x: Dict[(VirtualMachine, Service)]
 
-    def __init__(self, data: Data):
+    prob: LpProblem
+    objective: LpAffineExpression
+
+    base_data: BaseData
+    perf_data: Optional[PerformanceData]
+    multi_data: Optional[MultiCloudData]
+
+    vm_matching: Dict[(VirtualMachine, Service)]
+
+    def __init__(self, base_data: BaseData):
         """Create a new model for the cost optimization problem."""
-        self.data = data
+        self.base_data = base_data
         self.prob = LpProblem("cloud_cost_optimization", LpMinimize)
 
-        # == Variables ==
-
         # Assign virtual machine v to cloud service s?
-        self.x = {
-            (v, s): LpVariable(f"x_(v,s)")
-            for v in data.virtual_machines
-            for s in data.services
+        self.vm_matching = {
+            (v, s): LpVariable(f"vm_matching[{v},{s}]", cat=LpBinary)
+            for v in base_data.virtual_machines
+            for s in base_data.services
         }
 
-        # == Constraints ==
-
         # Assign each VM to exactly one cloud service
-        for v in data.virtual_machines:
-            self.prob += lpSum(self.x[v, s] for s in data.services) == 1
+        for v in base_data.virtual_machines:
+            self.prob += lpSum(self.vm_matching[v, s] for s in base_data.services) == 1
 
-        # == Objective ==
+        # Base costs for used virtual machines
+        self.objective = lpSum(
+            self.vm_matching[v, s] * base_data.service_base_costs[s]
+            for v in base_data.virtual_machines
+            for s in base_data.services
+        )
 
-        # Minimize costs
-        self.prob += lpSum(
-            self.x[v, s] * data.service_base_costs[s]
-            for v in data.virtual_machines
-            for s in data.services
+    def with_performance(self, perf_data: PerformanceData) -> Self:
+        """Add performance data to the model."""
+        self.perf_data = perf_data
+
+        # Enforce minimum performance constraints
+        for v in self.base_data.virtual_machines:
+            # RAM
+            self.prob += (
+                lpSum(
+                    self.vm_matching[v, s] * perf_data.service_ram[s]
+                    for s in self.base_data.services
+                )
+                >= perf_data.virtual_machine_min_ram[v]
+            )
+            # vCPUs
+            self.prob += (
+                lpSum(
+                    self.vm_matching[v, s] * perf_data.service_cpu_count[s]
+                    for s in self.base_data.services
+                )
+                >= perf_data.virtual_machine_min_cpu_count[v]
+            )
+
+        return self
+
+    def with_multi_cloud(self, multi_data: MultiCloudData) -> Self:
+        """Add multi cloud data to the model."""
+        self.multi_data = multi_data
+
+        # Is cloud service provider k used at all?
+        csp_used = {
+            k: LpVariable(f"csp_used[{k}]", cat=LpBinary)
+            for k in multi_data.cloud_service_providers
+        }
+
+        # Calculate csp_used values
+        for k in multi_data.cloud_service_providers:
+            used_service_count = lpSum(
+                self.vm_matching[v, s]
+                for v in self.base_data.virtual_machines
+                for s in multi_data.cloud_service_provider_services[k]
+            )
+            self.prob += csp_used[k] <= used_service_count
+            self.prob += (
+                csp_used[k] * len(multi_data.cloud_service_provider_services)
+                >= used_service_count
+            )
+
+        # Enforce minimum and maximum number of used CSPs
+        self.prob += (
+            lpSum(csp_used[k] for k in multi_data.cloud_service_providers)
+            >= multi_data.min_cloud_service_provider_count
+        )
+        self.prob += (
+            lpSum(csp_used[k] for k in multi_data.cloud_service_providers)
+            <= multi_data.max_cloud_service_provider_count
         )
 
     def solve(self) -> LpProblem:
         """Solve the optimization problem."""
+        # Add the objective function
+        self.prob += self.objective
+
         self.prob.solve()
         return self.prob
