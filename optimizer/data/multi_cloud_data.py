5e5cd2a591c2dcb614178c65b905cfdcfaeb7791
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-12-30 19:33:03 +0100

    Validate multi cloud data

--- optimizer/data/multi_cloud_data.py
+++ optimizer/data/multi_cloud_data.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import List, Dict
 
 from optimizer.data import Service, CloudServiceProvider
+from optimizer.data.base_data import BaseData
 
 
 @dataclass
@@ -17,3 +18,28 @@ class MultiCloudData:
 
     # The maximum number of cloud service providers that can be used
     max_cloud_service_provider_count: int
+
+    def validate(self, base_data: BaseData):
+        """Validate the data for consistency."""
+        # Validate cloud_service_provider_services
+        for csp in self.cloud_service_providers:
+            assert (
+                csp in self.cloud_service_provider_services.keys()
+            ), f"CSP {csp} is missing in cloud_service_provider_services"
+
+        for csp, services in self.cloud_service_provider_services.items():
+            assert (
+                csp in self.cloud_service_providers
+            ), f"{csp} in cloud_service_provider_services is not a valid CSP"
+
+            for s in services:
+                assert (
+                    s in base_data.services
+                ), f"{s} in cloud_service_provider_services is not a valid service"
+
+        assert (
+            self.min_cloud_service_provider_count >= 0
+        ), "min_cloud_service_provider_count is negative"
+        assert (
+            self.max_cloud_service_provider_count >= 0
+        ), "max_cloud_service_provider_count is negative"
