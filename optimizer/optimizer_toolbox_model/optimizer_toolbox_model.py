5f5c5fc0c7b883c169dc943068ca6661fea35dd8
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2023-02-09 14:35:14 +0100

    Add data class for verified optimizer toolbox model

--- optimizer/optimizer_toolbox_model/optimizer_toolbox_model.py
+++ optimizer/optimizer_toolbox_model/optimizer_toolbox_model.py
@@ -26,3 +26,32 @@ class OptimizerToolboxModel:
     def with_multi_cloud_data(self, multi_cloud_data: MultiCloudData) -> Self:
         """Add multi cloud data to the model."""
         self.multi_cloud_data = multi_cloud_data
+
+    def validate(self) -> "ValidatedOptimizerToolboxModel":
+        """Validate the model for integrity."""
+        # FIXME: Apply validation
+        return ValidatedOptimizerToolboxModel(self)
+
+
+@dataclass
+class ValidatedOptimizerToolboxModel:
+    """An optimizer toolbox model that has been validated for integrity."""
+    optimizer_toolbox_model: OptimizerToolboxModel
+
+    def __init__(self, optimizer_toolbox_model: OptimizerToolboxModel):
+        """
+        Create a new validated optimizer toolbox model.
+
+        This should never be created directly, unless the data has been verified at another place.
+        Instead, call the `.validate` function on the model.
+        """
+        self.optimizer_toolbox_model = optimizer_toolbox_model
+
+    def optimize(self) -> Self:
+        """
+        Optimize the model by applying pre-processing techniques.
+
+        If optimizations can be applied, this will result in faster solving times for the model.
+        """
+        # FIXME: Apply optimizations
+        return self
