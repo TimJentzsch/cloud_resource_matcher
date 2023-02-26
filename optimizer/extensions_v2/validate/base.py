ccbd2f86e5f57f3cf344b486ea0e8e54970d29dd
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2023-02-26 11:28:03 +0100

    Add new extension for data validation

---
+++ optimizer/extensions_v2/validate/base.py
@@ -0,0 +1,12 @@
+from optimizer.extensions.data import BaseData
+from optimizer.optimizer_v2.extension import Extension
+
+
+class ValidateBaseExt(Extension[None]):
+    base_data: BaseData
+
+    def __init__(self, base_data: BaseData):
+        self.base_data = base_data
+
+    def action(self) -> None:
+        self.base_data.validate()
---
+++ optimizer/extensions_v2/validate/performance.py
@@ -0,0 +1,14 @@
+from optimizer.extensions.data import PerformanceData, BaseData
+from optimizer.optimizer_v2.extension import Extension
+
+
+class ValidatePerformanceExt(Extension[None]):
+    base_data: BaseData
+    performance_data: PerformanceData
+
+    def __init__(self, base_data: BaseData, performance_data: PerformanceData):
+        self.base_data = base_data
+        self.performance_data = performance_data
+
+    def action(self) -> None:
+        self.performance_data.validate(self.base_data)
