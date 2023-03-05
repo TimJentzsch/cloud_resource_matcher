774705811238737e71604398bc716f74d72deae6
Tim Jentzsch <tim.jentzsch@rwth-aachen.de>
2023-03-05 21:02:30 +0100

    Add multi cloud package

---
+++ optimizer/packages/multi_cloud/validate.py
@@ -0,0 +1,14 @@
+from optimizer.data import MultiCloudData, BaseData
+from optimizer.workflow_engine import Task
+
+
+class ValidateMultiCloudTask(Task[None]):
+    base_data: BaseData
+    multi_cloud_data: MultiCloudData
+
+    def __init__(self, base_data: BaseData, multi_cloud_data: MultiCloudData):
+        self.base_data = base_data
+        self.multi_cloud_data = multi_cloud_data
+
+    def execute(self) -> None:
+        self.multi_cloud_data.validate(self.base_data)
