dc364262819b9d94b614ee5e44b88096688abbcc
Tim Jentzsch <commits@timjen.net>
2023-04-08 21:43:34 +0200

    Enforce performance constraints during pre-processing

--- optimizer/packages/performance/build_mip.py
+++ optimizer/packages/performance/build_mip.py
@@ -1,37 +1,10 @@
 from optiframe.framework.tasks import BuildMipTask
-from pulp import LpProblem
-
-from optimizer.packages.base import BaseData, BaseMipData
-
-from .data import PerformanceData
 
 
 class BuildMipPerformanceTask(BuildMipTask[None]):
-    base_data: BaseData
-    performance_data: PerformanceData
-    base_mip_data: BaseMipData
-    problem: LpProblem
-
-    def __init__(
-        self,
-        base_data: BaseData,
-        performance_data: PerformanceData,
-        base_mip_data: BaseMipData,
-        problem: LpProblem,
-    ):
-        self.base_data = base_data
-        self.performance_data = performance_data
-        self.base_mip_data = base_mip_data
-        self.problem = problem
+    def __init__(self) -> None:
+        pass
 
     def execute(self) -> None:
-        # Enforce performance limits for every cloud service
-        for (cr, pc), demand in self.performance_data.performance_demand.items():
-            for cs in self.base_data.cr_to_cs_list[cr]:
-                supply = self.performance_data.performance_supply.get((cs, pc))
-                if supply is not None:
-                    if supply < demand:
-                        self.problem += (
-                            self.base_mip_data.var_cr_to_cs_matching[cr, cs] == 0,
-                            f"performance_limit({cr},{cs},{pc})",
-                        )
+        """The constraints are enforced entirely in the pre-processing step."""
+        pass
