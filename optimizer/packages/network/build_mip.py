202efa2de32f97900fa76d5fb5abc62ceee8cc8d
Tim Jentzsch <commits@timjen.net>
2023-04-13 15:36:24 +0200

    Enforce max latency requirement for CR -> CR connections during pre-processing

--- optimizer/packages/network/build_mip.py
+++ optimizer/packages/network/build_mip.py
@@ -35,6 +35,68 @@ class BuildMipNetworkTask(BuildMipTask[NetworkMipData]):
         self.problem = problem
 
     def execute(self) -> NetworkMipData:
+        connection_deployments = dict()
+
+        # Pre-compute the possible deployments for CR pairs respecting the latency
+        for (
+            cr1,
+            cr2,
+        ) in self.network_data.cr_and_cr_to_traffic.keys():
+            max_latency = self.network_data.cr_and_cr_to_max_latency.get((cr1, cr2))
+            deployment_list = []
+
+            for cs1 in self.base_data.cr_to_cs_list[cr1]:
+                loc1 = self.network_data.cs_to_loc[cs1]
+
+                for cs2 in self.base_data.cr_to_cs_list[cr2]:
+                    loc2 = self.network_data.cs_to_loc[cs2]
+
+                    if (
+                        max_latency is None
+                        or self.network_data.loc_and_loc_to_latency[(loc1, loc2)] <= max_latency
+                    ):
+                        deployment_list.append((cs1, cs2))
+
+            connection_deployments[(cr1, cr2)] = deployment_list
+
+        # Is there a cr1 -> cr2 connection where cr1 is deployed to cs1 and cr2 to cs2?
+        var_cr_pair_cs_deployment: dict[
+            tuple[CloudResource, CloudService, CloudResource, CloudService],
+            LpVariable,
+        ] = {
+            (cr1, cs1, cr2, cs2): LpVariable(
+                f"cr_pair_cs_deployment({cr1},{cs1},{cr2},{cs2})",
+                cat=LpBinary,
+            )
+            for (cr1, cr2) in self.network_data.cr_and_cr_to_traffic.keys()
+            for (cs1, cs2) in connection_deployments[(cr1, cr2)]
+        }
+
+        # Calculate deployments of cloud resource pairs
+        for (
+            cr1,
+            cr2,
+        ) in self.network_data.cr_and_cr_to_traffic.keys():
+            # Every CR pair has one pair of cloud service connections
+            self.problem += (
+                lpSum(
+                    var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2]
+                    for (cs1, cs2) in connection_deployments[(cr1, cr2)]
+                )
+                == 1
+            )
+
+            # If a CR has been deployed to a given CS, enforce this for the pair as well
+            for (cs1, cs2) in connection_deployments[(cr1, cr2)]:
+                self.problem += (
+                    var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2]
+                    <= self.base_mip_data.var_cr_to_cs_matching[cr1, cs1]
+                )
+                self.problem += (
+                    var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2]
+                    <= self.base_mip_data.var_cr_to_cs_matching[cr2, cs2]
+                )
+
         # Pay for CR -> loc traffic
         self.problem.objective += lpSum(
             self.base_mip_data.var_cr_to_cs_matching[cr, cs]
@@ -48,65 +110,7 @@ class BuildMipNetworkTask(BuildMipTask[NetworkMipData]):
             for cs in self.base_data.cr_to_cs_list[cr]
         )
 
-        # Is there a cr1 -> cr2 connection where cr1 is deployed to cs1 and cr2 to cs2?
-        var_cr_pair_cs_deployment: dict[
-            tuple[CloudResource, CloudService, CloudResource, CloudService],
-            LpVariable,
-        ] = {
-            (cr1, cs1, cr2, cs2): LpVariable(
-                f"cr_pair_cs_deployment({cr1},{cs1},{cr2},{cs2})",
-                cat=LpBinary,
-            )
-            for (
-                cr1,
-                cr2,
-            ) in self.network_data.cr_and_cr_to_traffic.keys()
-            for cs1 in self.base_data.cr_to_cs_list[cr1]
-            for cs2 in self.base_data.cr_to_cs_list[cr2]
-        }
-
-        # Calculate deployments of cloud resource pairs
-        for (
-            cr1,
-            cr2,
-        ) in self.network_data.cr_and_cr_to_traffic.keys():
-            # Every CR pair has one pair of cloud service connections
-            self.problem += (
-                lpSum(
-                    var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2]
-                    for cs1 in self.base_data.cr_to_cs_list[cr1]
-                    for cs2 in self.base_data.cr_to_cs_list[cr2]
-                )
-                == 1
-            )
-
-            # If a CR has been deployed to a given CS, enforce this for the pair as well
-            for cs1 in self.base_data.cr_to_cs_list[cr1]:
-                for cs2 in self.base_data.cr_to_cs_list[cr2]:
-                    self.problem += (
-                        var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2]
-                        <= self.base_mip_data.var_cr_to_cs_matching[cr1, cs1]
-                    )
-                    self.problem += (
-                        var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2]
-                        <= self.base_mip_data.var_cr_to_cs_matching[cr2, cs2]
-                    )
-
-        # Respect maximum latencies for CR -> CR traffic
-        for (
-            cr1,
-            cr2,
-        ), max_latency in self.network_data.cr_and_cr_to_max_latency.items():
-            for cs1 in self.base_data.cr_to_cs_list[cr1]:
-                loc1 = self.network_data.cs_to_loc[cs1]
-
-                for cs2 in self.base_data.cr_to_cs_list[cr2]:
-                    loc2 = self.network_data.cs_to_loc[cs2]
-
-                    if self.network_data.loc_and_loc_to_latency[loc1, loc2] > max_latency:
-                        self.problem += var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2] == 0
-
-        # Pay for CR -> loc traffic caused by CR -> CR connections
+        # Pay for CR -> CR traffic
         self.problem.objective += lpSum(
             var_cr_pair_cs_deployment[cr1, cs1, cr2, cs2]
             * self.base_data.cr_to_instance_demand[cr1]
@@ -118,8 +122,7 @@ class BuildMipNetworkTask(BuildMipTask[NetworkMipData]):
                 cr1,
                 cr2,
             ), traffic in self.network_data.cr_and_cr_to_traffic.items()
-            for cs1 in self.base_data.cr_to_cs_list[cr1]
-            for cs2 in self.base_data.cr_to_cs_list[cr2]
+            for (cs1, cs2) in connection_deployments[(cr1, cr2)]
         )
 
         return NetworkMipData(var_cr_pair_cs_deployment)
